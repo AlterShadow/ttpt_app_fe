0a1af577f93e923348b4fcb1213da99e6baaf008
cupidbow20000 <hiroshi.tokudome0921@gmail.com>
2024-07-24 09:35:15 -0400

    sd

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -3,11 +3,11 @@ import { CiCircleMore } from "react-icons/ci";
 
 export default function Header() {
   return (
-    <div className="bg-black py-1 px-7 flex justify-between items-center my-4">
+    <div className="bg-black py-1 px-7 flex justify-start items-center my-4">
       <Link href="/" className="font-bold text-4 text-[#327AD9] leading-5">
         Cancel
       </Link>
-      <div className="flex flex-col justify-center items-center pl-10">
+      <div className="flex flex-col justify-center items-center pl-20">
         <div className="font-bold text-4 text-white leading-5 text-center">
           TRYTOPLAYTHAT
         </div>
@@ -16,7 +16,7 @@ export default function Header() {
         </div>
         
       </div>
-      <div> </div>
+      
     </div>
   );
 }
